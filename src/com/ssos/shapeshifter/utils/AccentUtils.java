1b661383e5c60ccbf152b6602a720cf72f5e6286
AshutoshSundresh <ashutoshsundresh@gmail.com>
2020-11-25 19:26:53 +0530

    Introduce framework for setting accents
    Based on fruity pebbles. Most of the code was adapted for our impl using ImageView and directly onCreate rather than dialog
    
    Thanks to Alex Cruz for most of the utils

---
+++ src/com/ssos/shapeshifter/utils/AccentUtils.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ssos.shapeshifter.utils;
+
+public class AccentUtils {
+
+    public static final String[] ACCENTS = {
+            "co.aospa.theme.color.teal", // Teal
+            "co.aospa.theme.color.supernova", // SuperNova
+            "co.aospa.theme.color.pixelblue", // PixelBlue
+            "com.android.theme.color.tangerine", // Tangerine
+            "com.android.theme.color.aquamarine", // Aquamarine
+            "com.android.theme.color.orchid", // Orchid
+            "com.android.theme.color.black", // Black
+            "com.android.theme.color.green", // Green
+            "com.android.theme.color.space", // Space
+            "co.aospa.theme.color.dodgerblue", // DodgerBlue
+            "co.aospa.theme.color.torchred", // TorchRed
+            "co.aospa.theme.color.magenta", // Magenta
+            "co.aospa.theme.color.red", // Red
+            "co.shapeshiftos.theme.color.cheese", // Cheese
+            "com.android.theme.color.miui", // MIUI
+    };
+}
