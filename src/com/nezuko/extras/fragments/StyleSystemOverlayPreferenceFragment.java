be4584c68f5ba7abbb34108588bb85b1b7fb7079
Maitreya29 <Maitreyapatni30@gmail.com>
2021-08-15 09:51:05 +0530

    Themer: Drop extras icon

--- src/com/nezuko/extras/fragments/StyleSystemOverlayPreferenceFragment.java
+++ src/com/nezuko/extras/fragments/StyleSystemOverlayPreferenceFragment.java
@@ -67,7 +67,6 @@ public class StyleSystemOverlayPreferenceFragment extends DashboardFragment impl
 
     private IOverlayManager mOverlayManager;
     private PackageManager mPackageManager;
-    private static final String OVERLAY_EXTRAS_ICONS = "overlay_extras_icons";
 
     private SystemSettingListPreference mExtraIcons;
     private Handler mHandler;
@@ -77,98 +76,6 @@ public class StyleSystemOverlayPreferenceFragment extends DashboardFragment impl
         super.onCreate(savedInstanceState);
         final ContentResolver resolver = getActivity().getContentResolver();
         Context mContext = getContext();
-        mOverlayManager = IOverlayManager.Stub.asInterface(
-                ServiceManager.getService(Context.OVERLAY_SERVICE));
-        mExtraIcons = (SystemSettingListPreference) findPreference(OVERLAY_EXTRAS_ICONS);
-
-        mCustomSettingsObserver.observe();
-    }
-
-    private CustomSettingsObserver mCustomSettingsObserver = new CustomSettingsObserver(mHandler);
-    private class CustomSettingsObserver extends ContentObserver {
-
-        CustomSettingsObserver(Handler handler) {
-            super(handler);
-        }
-
-        void observe() {
-            Context mContext = getContext();
-            ContentResolver resolver = mContext.getContentResolver();
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.OVERLAY_EXTRAS_ICONS),
-                    false, this, UserHandle.USER_ALL);
-        }
-
-        @Override
-        public void onChange(boolean selfChange, Uri uri) {
-            if (uri.equals(Settings.System.getUriFor(Settings.System.OVERLAY_EXTRAS_ICONS))) {
-                updateExtraIcons();
-            }
-        }
-    }
-
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mExtraIcons) {
-            mCustomSettingsObserver.observe();
-            return true;
-        }
-        return false;
-    }
-
-    private void updateExtraIcons() {
-        ContentResolver resolver = getActivity().getContentResolver();
-
-        boolean dashboardTint = Settings.System.getIntForUser(getContext().getContentResolver(),
-                Settings.System.OVERLAY_EXTRAS_ICONS, 0, UserHandle.USER_CURRENT) == 1;
-        boolean dashboardDefault = Settings.System.getIntForUser(getContext().getContentResolver(),
-                Settings.System.OVERLAY_EXTRAS_ICONS, 0, UserHandle.USER_CURRENT) == 0;
-
-        if (dashboardDefault) {
-            setDefaultExtrasDashboardIcons(mOverlayManager);
-        } else if (dashboardTint) {
-            enableTintExtraIcons(mOverlayManager, "com.android.theme.nezez.tint");
-        }
-    }
-
-    public static void setDefaultExtrasDashboardIcons(IOverlayManager overlayManager) {
-        for (int i = 0; i < DASHBOARD_ICONS.length; i++) {
-            String card = DASHBOARD_ICONS[i];
-            try {
-                overlayManager.setEnabled(card, false, USER_SYSTEM);
-            } catch (RemoteException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public static void enableTintExtraIcons(IOverlayManager overlayManager, String overlayName) {
-        try {
-            for (int i = 0; i < DASHBOARD_ICONS.length; i++) {
-                String overlayrro = DASHBOARD_ICONS[i];
-                try {
-                    overlayManager.setEnabled(overlayrro, false, USER_SYSTEM);
-                } catch (RemoteException e) {
-                    e.printStackTrace();
-                }
-            }
-            overlayManager.setEnabled(overlayName, true, USER_SYSTEM);
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static final String[] DASHBOARD_ICONS = {
-        "com.android.theme.nezez.tint"
-    };
-
-    public static void handleOverlays(String packagename, Boolean state, IOverlayManager mOverlayManager) {
-        try {
-            mOverlayManager.setEnabled(packagename,
-                    state, USER_SYSTEM);
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
