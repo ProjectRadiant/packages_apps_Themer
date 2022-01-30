40caa642a5eb59cee883b85e8de386cfd1b122d2
acras01 <acras1@gmail.com>
2022-01-30 19:04:33 +0530

    Radiant Extras: Allow toggling combined signal icons [2/2]
    * @/jhonboy121: remove preference controller usage and use the default value for switch from sysui resource
    idoybh (YAAP): Good ol' java
    
    [maitreya29: drop unnecessary boolean definitions and minor cleanup]
    
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Co-authored-by: idoybh <idoybh2@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- src/com/radiant/extras/fragments/StatusBarSettings.java
+++ src/com/radiant/extras/fragments/StatusBarSettings.java
@@ -33,23 +33,48 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Collections;
+import com.radiant.support.preferences.SecureSettingSwitchPreference;
 
 public class StatusBarSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
+    private static final String COMBINED_STATUSBAR_ICONS = "show_combined_status_bar_signal_icons";
+    private static final String CONFIG_RESOURCE_NAME = "flag_combined_status_bar_signal_icons";
+    private static final String SYSTEMUI_PACKAGE = "com.android.systemui";
+
+    SecureSettingSwitchPreference mCombinedIcons;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-
         addPreferencesFromResource(R.xml.radiant_extras_statusbar);
-
         PreferenceScreen prefSet = getPreferenceScreen();
-
+        mCombinedIcons = (SecureSettingSwitchPreference)
+                findPreference(COMBINED_STATUSBAR_ICONS);
+        Resources sysUIRes = null;
+        boolean def = false;
+        int resId = 0;
+        try {
+            sysUIRes = getActivity().getPackageManager()
+                    .getResourcesForApplication(SYSTEMUI_PACKAGE);
+        } catch (Exception ignored) {
+            // If you don't have system UI you have bigger issues
+        }
+        if (sysUIRes != null) {
+            resId = sysUIRes.getIdentifier(
+                    CONFIG_RESOURCE_NAME, "bool", SYSTEMUI_PACKAGE);
+            if (resId != 0) def = sysUIRes.getBoolean(resId);
+        }
+        mCombinedIcons.setChecked(Settings.Secure.getInt(getActivity().getContentResolver(), COMBINED_STATUSBAR_ICONS, def ? 1 : 0) == 1);
+        mCombinedIcons.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-
+        if(preference == mCombinedIcons){
+            Settings.Secure.putInt(getActivity().getContentResolver(), COMBINED_STATUSBAR_ICONS, (boolean) objValue ? 1 : 0);
+            return true;
+        }
         return false;
     }
 
